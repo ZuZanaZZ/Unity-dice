f93a413cf002ef910911f38a20add7d4866a9fac
ZuZanaZZ <zuzanazemlicka@gmail.com>
2025-12-16 18:30:19 +0100

    Fix direction mapping to correctly return top face of dice from bottom raycasting hit

--- Assets/Scripts/DiceController.cs
+++ Assets/Scripts/DiceController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 
 public class DiceController : MonoBehaviour
@@ -7,7 +8,7 @@ public class DiceController : MonoBehaviour
     public Rigidbody Dice;
     public void Roll(InputAction.CallbackContext context)
     {
-        // Add a random force and spin to the dice for realistic rolling
+        // add a random force and spin to the dice for realistic rolling
         // new input system tutorial: https://www.youtube.com/watch?v=qEtLamo_-_g
         float xForce = Random.Range(-50, 50);
         float yForce = Random.Range(100 , 150);
@@ -20,41 +21,44 @@ public class DiceController : MonoBehaviour
 
     void Update()
     {
-        // Raycast explanation: https://www.youtube.com/watch?v=cUf7FnNqv7U
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 2f, Color.red);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out RaycastHit hitInfo1, 2f))
-        {
-            Debug.Log("1");
+        // if dice is no longer rolling, get number on the face
+        if (Dice.linearVelocity == Vector3.zero){
+            int resultNumber = GetNumber();
+            Debug.Log(resultNumber);
         }
+    }
+
+    public int GetNumber()
+    {
+        // raycast explanation: https://www.youtube.com/watch?v=cUf7FnNqv7U
+        Vector3 dicePosition = transform.position;
+        float distance = 2f;
+        RaycastHit hitInfo;
+        Vector3[] faceNumbers =
+        {
+            transform.TransformDirection(Vector3.down),    // 6
+            transform.TransformDirection(Vector3.forward), // 5
+            transform.TransformDirection(Vector3.right),   // 4
+            transform.TransformDirection(Vector3.left),    // 3
+            transform.TransformDirection(Vector3.back),    // 2
+            transform.TransformDirection(Vector3.up)       // 1
+        };
         
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * 2f, Color.orange);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out RaycastHit hitInfo2, 2f))
+        int[] topFaces = { 1, 2, 3, 4, 5, 6 };
+
+        // cast ray in all face directions
+        for (int i = 0; i < faceNumbers.Length; i++)
         {
-            Debug.Log("2");
+            // if the ray hits the ground
+            if (Physics.Raycast(dicePosition, faceNumbers[i], out hitInfo, distance))
+            {
+                // Debug.DrawRay(dicePosition, directions[i] * distance, Color.red, 0.5f);
+                // return the number on the (opposite) top face
+                return topFaces[i];
+            }
         }
-        
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 2f, Color.yellow);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out RaycastHit hitInfo3, 2f))
-        {
-            Debug.Log("3");
-        }
-        
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 2f, Color.green);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out RaycastHit hitInfo4, 2f))
-        {
-            Debug.Log("4");
-        }
-        
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2f, Color.blue);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo5, 2f))
-        {
-            Debug.Log("5");
-        }
-        
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 2f, Color.violet);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out RaycastHit hitInfo6, 2f))
-        {
-            Debug.Log("6"); 
-        }
+
+        // no ray hit the ground plane
+        return -1; 
     }
 }

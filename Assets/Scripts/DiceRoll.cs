4bf2f9fe26c8a249d296f860f7acbb7aedb5a4ae
ZuZanaZZ <zuzanazemlicka@gmail.com>
2025-12-18 19:44:04 +0100

    Fix forces for more realistic rolling

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -14,12 +14,12 @@ public class DiceRoll : MonoBehaviour
         canRoll = false;
         // add a random force and spin to the dice for realistic rolling
         // new input system tutorial: https://www.youtube.com/watch?v=qEtLamo_-_g
-        float xForce = Random.Range(-50, 50);
-        float yForce = Random.Range(100 , 150);
-        float zForce = Random.Range(-50, 50);
+        float xForce = Random.Range(-300, 300);
+        float yForce = Random.Range(2000 , 3000);
+        float zForce = Random.Range(-300, 300);
         Dice.AddForce(xForce, yForce, zForce);
 
-        float Rotation = Random.Range(-30, 30);
+        float Rotation = Random.Range(-300, 300);
         Dice.AddTorque(Rotation, Rotation, Rotation);
 
         StartCoroutine(StopFlying());
@@ -28,7 +28,7 @@ public class DiceRoll : MonoBehaviour
     private IEnumerator StopFlying()
     {
         // wait to prevent the dice flying of screen
-        yield return new WaitForSecondsRealtime(0.3f);
+        yield return new WaitForSecondsRealtime(0.5f);
 
         // reset flag
         canRoll = true;

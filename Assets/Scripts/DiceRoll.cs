8aba910e81d661ea010b6a99fa7ba4e97e119b8e
ZuZanaZZ <zuzanazemlicka@gmail.com>
2025-12-18 17:33:54 +0100

    Feat decreased cooldown on rolling the dice

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -6,12 +6,12 @@ using UnityEngine.InputSystem;
 public class DiceRoll : MonoBehaviour
 {
     public Rigidbody Dice;
-    private bool inAir = false;
+    private bool canRoll = true;
     public void Roll(InputAction.CallbackContext context)
     {
         // if the dice is already in the air, don't roll again
-        if (inAir) return;
-        inAir = true;
+        if (!canRoll) return;
+        canRoll = false;
         // add a random force and spin to the dice for realistic rolling
         // new input system tutorial: https://www.youtube.com/watch?v=qEtLamo_-_g
         float xForce = Random.Range(-50, 50);
@@ -27,10 +27,10 @@ public class DiceRoll : MonoBehaviour
 
     private IEnumerator StopFlying()
     {
-        // wait until the dice has stopped flying
-        yield return new WaitUntil(() => Dice.linearVelocity == Vector3.zero) ;
+        // wait to prevent the dice flying of screen
+        yield return new WaitForSecondsRealtime(0.3f);
 
         // reset flag
-        inAir = false;
+        canRoll = true;
     }
 }
